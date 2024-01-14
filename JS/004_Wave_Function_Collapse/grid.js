47a7f7473235ca665b612c4669c8b8574d30715b
SrAquaGenius <dimoisivic@gmail.com>
2024-01-14 15:32:50 +0000

    Wave function collapse working

---
+++ JS/004_Wave_Function_Collapse/grid.js
@@ -0,0 +1,117 @@
+/* ----------------------------------------------------------------------------
+ * File:     grid.js
+ * Authors:  SrAqua
+ * Modified: 2024-01-14 10:27
+ * ------------------------------------------------------------------------- */
+
+/* -------------------------------- Globals -------------------------------- */
+
+const DIM = 40;
+
+let grid = [];
+
+/* ------------------------------- Cell class ------------------------------ */
+
+class Cell {
+
+	constructor(value) {
+
+		this.collapsed = false;
+		
+		if (value instanceof Array)	this.options = value;
+		else this.options = new Array(value).fill(0).map((_, i) => i);
+	}
+}
+
+/* ------------------------------- Functions ------------------------------- */
+function createGrid() {
+
+	// Create a cell for each spot in the grid
+	for (let i = 0; i < DIM * DIM; i++) 
+		grid[i] = new Cell(tiles.length);
+
+}
+
+function drawGrid() {
+	const w = width / DIM;
+	const h = height / DIM;
+
+	for (let j = 0; j < DIM; j++) {
+		for (let i = 0; i < DIM; i++) {
+
+			let cell = grid[i + j * DIM];
+
+			if (cell.collapsed) {
+				let index = cell.options[0];
+				image(tiles[index].img, i * w, j * h, w, h);
+				continue;
+			}
+
+			fill(0);
+			rect(i * w, j * h, w, h);
+		}
+	}
+}
+
+function generateNewGrid() {
+	const newGrid = [];
+	for (let j = 0; j < DIM; j++) {
+		for (let i = 0; i < DIM; i++) {
+
+			let index = i + j * DIM;
+			if (grid[index].collapsed) {
+				newGrid[index] = grid[index];
+				continue;
+			}
+
+			let options = new Array(tiles.length).fill(0).map((_, i) => i);
+
+			// Look up
+			if (j > 0) {
+				let up = grid[i + (j-1) * DIM];
+				let validOptions = [];
+				for (let option of up.options) {
+					let valid = tiles[option].down;
+					validOptions = validOptions.concat(valid);
+				}
+				checkValid(options, validOptions);
+			}
+
+			// Look right
+			if (i < DIM - 1) {
+				let right = grid[i + 1 + j * DIM];
+				let validOptions = [];
+				for (let option of right.options) {
+					let valid = tiles[option].left;
+					validOptions = validOptions.concat(valid);
+				}
+				checkValid(options, validOptions);
+			}
+			
+			// Look down
+			if (j < DIM - 1) {
+				let down = grid[i + (j+1) * DIM];
+				let validOptions = [];
+				for (let option of down.options) {
+					let valid = tiles[option].up;
+					validOptions = validOptions.concat(valid);
+				}
+				checkValid(options, validOptions);
+			}
+			
+			// Look left
+			if (i > 0) {
+				let left = grid[i - 1 + j * DIM];
+				let validOptions = [];
+				for (let option of left.options) {
+					let valid = tiles[option].right;
+					validOptions = validOptions.concat(valid);
+				}
+				checkValid(options, validOptions);
+			}
+
+			newGrid[index] = new Cell(options);
+		}
+	}
+	grid = newGrid;
+}
--- JS/004_Wave_Function_Collapse/tile.js
+++ JS/004_Wave_Function_Collapse/tile.js
@@ -1,3 +1,18 @@
+/* ----------------------------------------------------------------------------
+ * File:     tile.js
+ * Authors:  SrAqua
+ * Modified: 2024-01-14 10:26
+ * ------------------------------------------------------------------------- */
+
+/* -------------------------------- Globals -------------------------------- */
+
+const N_TILES = 13;
+
+const tiles = [];
+const tilesImages = [];
+
+/* ------------------------------- Tile class ------------------------------ */
+
 class Tile {
   constructor(img, edges) {
     this.img = img;
@@ -13,19 +28,19 @@ class Tile {
     for (let i = 0; i < tiles.length; i++) {
       let tile = tiles[i];
       // Up
-      if (tile.edges[2] == this.edges[0]) {
+      if (compareEdge(tile.edges[2], this.edges[0])) {
         this.up.push(i);
       }
       // Right
-      if (tile.edges[3] == this.edges[1]) {
+      if (compareEdge(tile.edges[3], this.edges[1])) {
         this.right.push(i);
       }
       // Down
-      if (tile.edges[0] == this.edges[2]) {
+      if (compareEdge(tile.edges[0], this.edges[2])) {
         this.down.push(i);
       }
       // Left
-      if (tile.edges[1] == this.edges[3]) {
+      if (compareEdge(tile.edges[1], this.edges[3])) {
         this.left.push(i);
       }
     }
@@ -52,4 +67,55 @@ class Tile {
     // Create and return a new tile with the new configurations
     return new Tile(newImg, newEdges);
   }
+}
+
+/* ------------------------------- Functions ------------------------------- */
+
+function reverseString(s) {
+	let arr = s.split("");
+	arr = arr.reverse();
+	return arr.join("");
+}
+
+function compareEdge(a, b) {
+	return a == reverseString(b);
+}
+
+function loadTiles() {
+
+	const path = "circuit";
+	for (let i = 0; i < N_TILES; i++)
+		tilesImages[i] = loadImage(`${path}/${i}.png`);
+}
+
+function createTiles() {
+
+	// Load and create the possible tiles
+	// A - dark grey; B - dark green, C - light green, D - light grey
+	tiles[0] = new Tile(tilesImages[0], ["AAA", "AAA", "AAA", "AAA"]);
+	tiles[1] = new Tile(tilesImages[1], ["BBB", "BBB", "BBB", "BBB"]);
+	tiles[2] = new Tile(tilesImages[2], ["BBB", "BCB", "BBB", "BBB"]);
+	tiles[3] = new Tile(tilesImages[3], ["BBB", "BDB", "BBB", "BDB"]);
+	tiles[4] = new Tile(tilesImages[4], ["ABB", "BCB", "BBA", "AAA"]);
+	tiles[5] = new Tile(tilesImages[5], ["ABB", "BBB", "BBB", "BBA"]);
+	tiles[6] = new Tile(tilesImages[6], ["BBB", "BCB", "BBB", "BCB"]);
+	tiles[7] = new Tile(tilesImages[7], ["BDB", "BCB", "BDB", "BCB"]);
+	tiles[8] = new Tile(tilesImages[8], ["BDB", "BBB", "BCB", "BBB"]);
+	tiles[9] = new Tile(tilesImages[9], ["BCB", "BCB", "BBB", "BCB"]);
+	tiles[10] = new Tile(tilesImages[10], ["BCB", "BCB", "BCB", "BCB"]);
+	tiles[11] = new Tile(tilesImages[11], ["BCB", "BCB", "BBB", "BBB"]);
+	tiles[12] = new Tile(tilesImages[12], ["BBB", "BCB", "BBB", "BCB"]);
+
+	// Generate rotations (some of then may be redundant)
+	for (let i = 2; i < N_TILES; i++) {
+		for (let j = 0; j < 4; j ++) {
+		tiles.push(tiles[i].rotate(j));
+		}
+	}
+
+	// Generate the adjacency rules based on edges
+	for (let i = 0; i < tiles.length; i++) {
+		const tile = tiles[i];
+		tile.analyze(tiles);
+	}
 }
